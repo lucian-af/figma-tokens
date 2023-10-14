5087e21396ddfde688fb218919c5e03ea861f75f
lucian-af <lucian-af@users.noreply.github.com>
2023-10-14 20:47:57 +0000

    style: Update design tokens

--- build/js/tokens.js
+++ build/js/tokens.js
@@ -1,13 +1,15 @@
 /**
  * Do not edit directly
- * Generated on Sat, 14 Oct 2023 20:05:51 GMT
+ * Generated on Sat, 14 Oct 2023 20:47:56 GMT
  */
 
 module.exports = {
+  "ColorRed300": "#fca5a5",
   "ColorRed500": "#ef4444",
   "ColorGray800": "#27272a",
   "ColorGray900": "#18181b",
   "ColorGray950": "#09090b",
+  "ColorWhite50": "#fafafa",
   "ColorWhite100": "#d4d4d8",
   "ColorWhite300": "#d4d4d8"
 };
