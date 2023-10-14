c7dc12781f510a58a7fb2804cbc35417e6082d03
Lucian AF <lucian_af@hotmail.com>
2023-10-14 17:11:23 -0300

    chore: adiciona configuração para geração de scripts

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Sat, 14 Oct 2023 20:05:51 GMT
+ */
+
+module.exports = {
+  "ColorRed500": "#ef4444",
+  "ColorGray800": "#27272a",
+  "ColorGray900": "#18181b",
+  "ColorGray950": "#09090b",
+  "ColorWhite100": "#d4d4d8",
+  "ColorWhite300": "#d4d4d8"
+};
